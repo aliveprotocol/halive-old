acf3d37df2a373d2a4e8a529afbb0229ead104a8
techcoderx <engtianxi@outlook.com>
2021-01-23 12:52:46 +0800

    append event playlist type for ongoing streams

--- src/main.js
+++ src/main.js
@@ -32,7 +32,10 @@ app.get('/stream/:author/:link', (req,res) => {
             return res.status(404).send()
 
         // video on demand if livestream ended
-        if (stream.ended) m3u8File += '\n#EXT-X-PLAYLIST-TYPE:VOD'
+        if (stream.ended)
+            m3u8File += '\n#EXT-X-PLAYLIST-TYPE:VOD'
+        else
+            m3u8File += '\n#EXT-X-PLAYLIST-TYPE:EVENT'
         m3u8File += '\n\n'
 
         if (stream.len) for (let c = 0; c < stream.len.length; c++) {
